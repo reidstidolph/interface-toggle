0914805755c50e2aefc59bdce003e2af4e736b9e
Reid Stidolph <rstidolph@128technology.com>
2017-06-03 23:31:28 -0700

    bunch of changes

--- interface.js
+++ interface.js
@@ -4,16 +4,17 @@
 */
 
 const fs = require('fs');              // Requires the fs module to read interface state from file system
-const iface="enp0s3";                  // Interface to control
+const iface="enp0s8";                  // Interface to control
 const task = require('child_process'); // Set up a shortcut var to spawn child processes
 
 /**
 * Fetch interface state.
 * @function
 * @param {string} intf - The name of an interface.
-* @returns {Promise} promise for results of interface state.
+* @returns {Promise} Promise for results of interface state.
 */
 function getInterfaceState(intf){
+
   return new Promise(
     (resolve, reject)=> {
       fs.readFile(`/sys/class/net/${intf}/operstate`, 'utf8', (err, newInterfaceState) => {
@@ -30,73 +31,102 @@ function getInterfaceState(intf){
 }
 
 /**
-* Turn interface up
+* Run a command in the OS.
 * @function
-* @param {string} intf - The name of an interface.
+* @param {string} cmd - The name the command to run.
+* @param {Array<string>} args - Command arguments.
+* @returns {Promise} Promise for results of the command.
 */
-function intUp(intf){
-  task.execFile('ip', ['link', 'set', intf, 'up'], (error, stdout, stderr) => {
-    if (error) {
-      console.error('stderr', stderr);
-    } else {
-      console.log('Success!', stdout);
-    }
-  });
-}
-
-/**
-* Turn interface down
-* @function
-* @param {string} intf - The name of an interface.
-*/
-function intDown(intf){
-  task.execFile('ip', ['link', 'set', intf, 'down'], (error, stdout, stderr) => {
-    if (error) {
-      console.error('stderr', stderr);
-    } else {
-      console.log('Success!', stdout);
-    }
-  });
-}
-
-/**
-* Toggle interface up or down
-* @function
-* @param {string} intf - The name of an interface.
-*/
-function intToggle(intf){
-
-  console.log(`Toggle ${intf}`);
-
-  getInterfaceState(intf).then(
-    (state)=>{
-      if (state == 'up') {
-        console.log("Interface up...turning down.");
-        intDown(intf);
-      } else if (state == 'down') {
-        console.log("Interface down...turning up.");
-        intUp(intf);
+function osTask(cmd, args = [] ){
+  return new Promise(
+    (resolve, reject)=> {
+      if (!Array.isArray(args)) {
+        reject('args not an array.');
+      } else {
+        task.execFile(cmd, args, (error, stdout, stderr) => {
+          if (error) {
+            //console.log(error);
+            reject(stderr);
+          } else {
+            resolve(stdout);
+          }
+        });
       }
-    },
-    (reason)=>{
-      console.error('Something went wrong', reason);
     }
   );
 }
 
+
+/**
+* Manipulate interface state
+* @function
+* @param {string} intf - The name of an interface.
+* @param {string} state - up or down state to apply to interface.
+*/
 function intSet(intf, state) {
-  if (state === "up") {
-    intUp(intf);
-  } else if (state === "down") {
-    intDown(intf);
-  } else {
-    intToggle(intf);
+  // state is supplied, use it
+  if (state === "up" || state === "down") {
+    console.log(`Turning ${intf} ${state}.`);
+    // return the osTask promise
+    return osTask('ip', ['link', 'set', intf, state]);
+  }
+  // state not supplied. toggle the interface based on current state
+  else {
+    // return the getInterfaceState promise, with the osTask promise nested
+    return getInterfaceState(intf)
+      .then(
+        (state)=>{
+          if (state == 'up') {
+            console.log("Interface up...turning down.");
+            return osTask('ip', ['link', 'set', intf, 'down'])
+              .then((output)=>{return output;})
+              .catch((error)=>{return error;});
+          } else if (state == 'down') {
+            console.log("Interface down...turning up.");
+            return osTask('ip', ['link', 'set', intf, 'up'])
+              .then((output)=>{return output;})
+              .catch((error)=>{return error;});
+          }
+        }
+      )
+      .catch(
+        (reason)=>{
+          console.error('Something went wrong', reason);
+          reject(reason);
+        }
+      );
   }
 }
 
-intToggle(iface);
+intSet(iface)
+.then(
+  (output)=> {
+    console.log(output);
+  }
+)
+.catch(
+  (error)=> {
+    console.log(error);
+  }
+);
+
 
 module.exports = {
-  "get" : getInterfaceState(intf),
-  "set" : intSet(intf, state)
+  get : (callback)=>{
+    getInterfaceState(iface)
+      .then(
+        (results)=> {
+          callback(null, {state: results});
+        }
+      )
+      .catch(
+        (error)=> {
+          callback(error);
+        }
+      );
+  },
+  set : (callback)=>{
+    // initialize callback error to null
+    var error = null;
+  }
 }
