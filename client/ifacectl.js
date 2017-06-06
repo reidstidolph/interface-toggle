b167fd0cbb52f255bb3674750a0a97a531f2b112
Reid Stidolph <rstidolph@128technology.com>
2017-06-06 06:50:13 -0700

    fix client js

--- client/ifacectl.js
+++ client/ifacectl.js
@@ -1,13 +1,28 @@
 
 var iface = {
   get : function(iface, callback){
+    // set up new REST request
     var rest = new XMLHttpRequest();
+    // handle results that come back
     rest.onreadystatechange = function() {
+      // request is successful
       if (this.readyState == 4 && this.status == 200) {
-       console.log(this);
+        // try to parse response into JSON
+        try {
+          var r = JSON.parse(this.response);
+          callback(null, r);
+        } catch (e) {
+          console.log("Results parse error: " + e);
+          callback(e, null);
+        }
+      // request failed
+      } else {
+        callback("request failed with " + this.status, null);
       }
     };
+    // finish configuring REST request
     rest.open("GET", "/api/interface/" + iface, true);
+    // fire in the hole!
     rest.send();
   },
   set : function(iface, state, callback){
