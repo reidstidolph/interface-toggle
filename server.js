fa2d9dd006d7d75beba488bd86b34c1328886d98
rstidolph <rstidolph@128technology.com>
2017-06-06 10:09:23 -0700

    cleanup

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 "use strict";
 
 // call the packages we need
-const express    = require('express');        // call express
-const app        = express();                 // define our app using express
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 8080;        // set our port
-const iface = require('./ifacectl');
+const express    = require('express');      // call express
+const app        = express();               // define our app using express
+const bodyParser = require('body-parser');  // uses body-parser  express middleware
+const router = express.Router();            // get an instance of the express Router
+const port = process.env.PORT || 8080;      // set our port
+const iface = require('./ifacectl');        // interface controller
 
 // Map of interfaces available to be managed. Key is the name of the interace
 // available to the front-end. Value is the interface name in in the back-end.
@@ -14,19 +15,23 @@ const ifaceMap = new Map([
   ['int2', 'enp0s9']
 ]);
 
-// parse application/json
-app.use(bodyParser.json());
-app.use(express.static('client')); // serve client HTML
+app.use(bodyParser.json());                 // parse application/json
+app.use(express.static('client'));          // serve client HTML
 
-// ROUTES FOR OUR API
-// =============================================================================
-const router = express.Router();              // get an instance of the express Router
+/* ROUTES FOR OUR API
+*  =============================================================================
+*/
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// test route to make sure everything is working (GET /api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'success!' });
 });
 
+/* REST resource /api/interface
+*  =============================================================================
+*  This resource accpets GET method, and returns an array containing the
+*  interfaces that are available for control.
+*/
 router.route('/interface')
   .get((req, res)=> {
 
@@ -35,7 +40,32 @@ router.route('/interface')
     res.json(Array.from(ifaceMap.keys()));
   })
 
-// route to resource for sepecific interface
+/* REST resource /api/interface/<some interface>
+*  =============================================================================
+*  This resource accepts GET method with a valid interface name, and returns the
+*  current up|down state.
+*
+*  It also accepts POST method with a valid interface name, to set the state of
+*  the interface.
+*
+*  If POST has no valid URL parameters or valid request body, it toggles the
+*  interface.
+*
+*  If POST contains a URL parameter for state up|down, it sets state
+*  accordingly.
+*    e.g. POST /api/interface/myinterface?state=down
+*
+*  If POST contains a request body containing a JSON object with state, it sets
+*  state accordingly.
+*    e.g. POST /api/interface/myinterface body={"state":"down"}
+*
+*  If a POST contains both a valid URL parameter and a valid body, the URL
+*  parameter takes precedence.
+*
+*  In all cases of a successful POST or GET, state of the interface is returned
+*  as JSON in the response body.
+*    e.g. {"state":"down"} or {"state":"up"}
+*/
 router.route('/interface/:ifaceName')
   .get((req, res)=> {
     console.log("request to get interface state.");
@@ -102,11 +132,14 @@ router.route('/interface/:ifaceName')
     }
   })
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+/* REGISTER OUR ROUTES
+*  =============================================================================
+*  all of our routes will be prefixed with /api
+*/
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
+/* START THE SERVER
+*  =============================================================================
+*/
 app.listen(port);
 console.log('Magic happens on port ' + port);
