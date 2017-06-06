edc21da1d7062519a8414c43c69eafed612bcd5a
Reid Stidolph <rstidolph@128technology.com>
2017-06-06 07:14:26 -0700

    fix client js

--- client/ifacectl.js
+++ client/ifacectl.js
@@ -1,12 +1,14 @@
-
 var iface = {
+  /**
+  *  object method to get interface state
+  */
   get : function(iface, callback){
     // set up new REST request
     var rest = new XMLHttpRequest();
     // handle results that come back
     rest.onreadystatechange = function() {
       // request is successful
-      if (this.readyState == 4 && this.status == 200) {
+      if (this.readyState === 4 && this.status === 200) {
         // try to parse response into JSON
         try {
           var r = JSON.parse(this.response);
@@ -16,7 +18,7 @@ var iface = {
           callback(e, null);
         }
       // request failed
-    } else if (this.readyState == 4 && this.status != 200) {
+    } else if (this.readyState === 4 && this.status != 200) {
         callback("request failed with " + this.status, null);
       }
     };
@@ -25,7 +27,37 @@ var iface = {
     // fire in the hole!
     rest.send();
   },
+/**
+*  object method to set interface state
+*/
   set : function(iface, state, callback){
+    // set up new REST request
+    var rest = new XMLHttpRequest();
+    // handle results that come back
+    rest.onreadystatechange = function() {
+      // request is successful
+      if (this.readyState === 4 && this.status === 200) {
+        // try to parse response into JSON
+        try {
+          var r = JSON.parse(this.response);
+          callback(null, r);
+        } catch (e) {
+          console.log("Results parse error: " + e);
+          callback(e, null);
+        }
+      // request failed
+    } else if (this.readyState === 4 && this.status != 200) {
+        callback("request failed with " + this.status, null);
+      }
+    };
+    // finish configuring REST request
+    if (state === "up" || state === "down") {
+      rest.open("POST", "/api/interface/" + iface + "?state=" + state, true);
+    } else {
+      rest.open("POST", "/api/interface/" + iface, true);
+    }
 
+    // fire in the hole!
+    rest.send();
   }
 }
